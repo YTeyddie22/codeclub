bee1a16c70467f56b0dd7455b105eba9a2f1bd70
Teddy Yongo <teddy@aeeiee.com>
2023-09-19 19:17:08 +0300

    code refactoring and implementing a continous program

--- caesar_cypher/main.py
+++ caesar_cypher/main.py
@@ -1,20 +1,35 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n");
-text = input("Type your message:\n").lower();
-shift = int(input("Type the shift number:\n"));
 
-#Cryptogragphy
-
-def cryptograph(direction, text_input,shift):
+should_continue = True;
+def cryptograph(direction, text_input, shift):
     text = "";
-    for letter in text_input:
-        position = alphabet.index(letter);
-        if direction == "encode":
+    
+    if direction == "decode":
+        shift *= -1;
+        
+    for char in text_input:
+        
+        if char in alphabet:
+            position = alphabet.index(char);
             new_position = position + shift;
-        elif direction == "decode":
-            new_position = position - shift;
-        new_letter = alphabet[new_position];
-        text += new_letter;
-    print(f"The decoded text is {text}")
-cryptograph(direction = direction, text_input = text, shift = shift);
+            text += alphabet[new_position];
+        else:
+            text += char;
+ 
+    print(f"The {direction}d text is {text}")
+    
+while should_continue:
+    #Cryptogragphy
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n");
+    text = input("Type your message:\n").lower();
+    shift = int(input("Type the shift number:\n"));
+
+    shift = shift % 26;
+    cryptograph(direction = direction, text_input = text, shift = shift); 
+    
+    result = input("Type 'yes if you want to continue but no if not'").lower();
+    
+    if result == 'no':
+        should_continue = False;
+        print("Goodbye");
