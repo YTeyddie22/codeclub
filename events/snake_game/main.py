4c12b299f3a647c414b431adc78da58dbcc4632a
Teddy Yongo <teyddie@gmail.com>
2024-03-06 10:45:44 +0300

    turtle race and oop for snake game

---
+++ events/snake_game/main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+from snake import Snake
+
+import time
+
+#Init the Screen
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+#Create the snake object
+snake = Snake()
+
+#Listen to key binds
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+#Init the Game
+game_is_on = True
+
+#Checking is the game is continuing and moving snake body
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+screen.exitonclick()
