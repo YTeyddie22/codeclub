dd07a4991e9395908f964b516650f37ffaf60d36
Teddy Yongo <teyddie@gmail.com>
2024-04-09 10:19:29 +0300

    collision with the wall

--- events/pong/main.py
+++ events/pong/main.py
@@ -1,10 +1,11 @@
 from turtle import Screen
-
+import time
 from paddle import Paddle
+from ball import Ball
 
 game_is_on = True;
-screen = Screen();
 
+screen = Screen();
 screen.tracer(0)
 screen.bgcolor("black");
 screen.setup(width=800, height=600)
@@ -13,8 +14,7 @@ screen.title("Pong")
 right_paddle = Paddle((350,0))
 left_paddle = Paddle((-350,0))
 
-
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
@@ -25,7 +25,12 @@ screen.onkey(left_paddle.go_down, "s")
 
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    
+    if(ball.ycor() > 280 or ball.ycor() < -280):
+        ball.bounce()
 
 
 screen.exitonclick()
