c6b3e5379bf98c50216f7b1bf001957698a19ec7
YTeyddie22 <teyddie@gmail.com>
2025-01-18 12:11:47 +0300

    feat: save the password on user own file

--- password_manager/main.py
+++ password_manager/main.py
@@ -3,13 +3,26 @@ import os
 
 absolute_path = os.path.dirname(__file__)
 img_name = "logo.png"
+file_name = "data.txt"
 image_path = os.path.join(absolute_path, img_name)
+data_path = os.path.join(absolute_path,file_name )
+
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    with open(data_path, "a") as data_file:
+        data_file.write(f"{website}| {email} | {password}\n")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -35,8 +48,10 @@ password_label.grid(row=3,column=0)
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1,columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "teyddie63@gmail.com")
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
@@ -44,7 +59,7 @@ password_entry.grid(row=3, column=1, columnspan=2)
 
 generate_password_button = Button(text="Generate Password", width=15)
 generate_password_button.grid(row=3,column=2)
-add_button = Button(text="Add", width=32)
+add_button = Button(text="Add", width=32, command=save)
 add_button.grid(row=4,column=1, columnspan=2)
 
 window.mainloop()
